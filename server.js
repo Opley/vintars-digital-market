829be8fc2cedb5ef6a4ce9937c1dcec6c0eae411
Opley Matootz <ilokanoprogrammer@gmail.com>
2022-03-14 07:35:01 +0100

    changed: port being used; set: process.env.PORT || 5000

--- server.js
+++ server.js
@@ -13,8 +13,10 @@ mongoose.connect(db).then(() => {
   console.log("connected to database...");
 });
 
+const port = process.env.PORT || 5000;
+
 //===========Server
-app.listen(3000, "127.0.0.1", () => {
+app.listen(port, "127.0.0.1", () => {
   console.log("server is listening on port 3000");
 });
 
